e6cd8466c357886795545df0f4ba48f04c151861
Travis Roberts <travis.c.roberts35@gmail.com>
2019-11-08 17:55:41 -0700

    added a few more methods for readouts, continuing switch

--- src/RemoteControlApp.java
+++ src/RemoteControlApp.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
+
 public class RemoteControlApp {
 
 	public static void main(String[] args) {
 		Scanner keyboard = new Scanner(System.in);
 		String choice = "";
+		String num = "";
 		
 		do {
 			initialMessage();
@@ -12,19 +14,44 @@ public class RemoteControlApp {
 
 		} while (!choice.equalsIgnoreCase("on"));
 		turnOn();
-		}
+		enterNumber();
 	
+		
+		
+		do {
+			num = keyboard.next();
+			switch(num) {
+				case "0":
+					pressButton();
+					enterNumber();
+					break;
+				case "1":
+					pressButton();
+					enterNumber();
+					break;
+			}
+		} while (!num.equalsIgnoreCase("10"));
+		
+		
+		}
+
 	public static void initialMessage() {
 		System.out.println("To begin, turn on the remote control.");
 	}
+
 	public static void turnOn() {
 		System.out.println("Turning On...");
 	}
-		
-		
 
+	public static void pressButton() {
+		System.out.println("BOOP!");
+	}
+	public static void enterNumber() {
+		System.out.println("Please enter a button number: ");
+		
 	}
 
-	
-	// TODO add pressButton() method
-	// TODO add turnOn() method
+}
+
+// TODO add pressButton() method
+// TODO add turnOn() method
