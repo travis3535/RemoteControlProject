dc29801e18142d5e2308163ae86fb9a8b2a6aa2e
Travis Roberts <travis.c.roberts35@gmail.com>
2019-11-08 17:12:54 -0700

    added turnon, initial message, and started do while

--- src/RemoteControlApp.java
+++ src/RemoteControlApp.java
@@ -1,7 +1,34 @@
-
+import java.util.Scanner;
 public class RemoteControlApp {
 
 	public static void main(String[] args) {
+		initialMessage();
+		Scanner keyboard = new Scanner(System.in);
+		String choice = "";
+		
+		do {
+			choice = keyboard.next();
+			switch(choice) {
+				
+				
+				
+			
+				
+				
+			}
+		} while (!choice.equalsIgnoreCase("on"));
+		
+		
+		
+	
+		}
+	public static void initialMessage() {
+		System.out.println("To begin, turn on the remote control.");
+	}
+	public static void turnOn() {
+		System.out.println("Turning On...");
+	}
+		
 		
 
 	}
@@ -9,4 +36,3 @@ public class RemoteControlApp {
 	
 	// TODO add pressButton() method
 	// TODO add turnOn() method
-}
